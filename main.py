ad2825f97fa8331ffa2565d85e60e5e13b3f67d0
akbhagasra <69315191+akbhagasra@users.noreply.github.com>
2022-05-26 14:26:12 +0530

    Added for deployment check

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+from datetime import date
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Welcome to API Exploration</p>"
+
+@app.route("/favNumber/<int:n>")
+def favNumber(n):
+    result = {
+        "My Favourite Number" : n
+    }
+    return jsonify(result)
+
+@app.route("/dateToday")
+def dateToday():
+    result = {
+        "Today's Date" : date.today().strftime("%B %d, %Y")
+    }
+    return jsonify(result)
+
+@app.route("/getContacts")
+def getContacts():
+    cnxn = mysql.connector.connect(
+    user='explore_user@explore-01',
+    password='1qweASD!', 
+    host='explore-01.mysql.database.azure.com', 
+    database='exploreDataBase')
+    cursor = cnxn.cursor()
+    query = 'SELECT * FROM Contacts'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cnxn.close()
+    return jsonify(result)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
